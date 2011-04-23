0b57317d14a2c3461486b243b52caf144f123ced
Andre Oschadlin <andreoschadlin@gmail.com>
2011-04-23 16:08:56 -0700

    Request handlers added

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var http = require('http')
   , url = require('url');
 
-function start(route) {
-  function handle(request, response) {
+function start(route, handle) {
+  function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log('Request received for ' + pathname);
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('Hello World');
     response.end();
   }
 
-  http.createServer(handle).listen(8888);
+  http.createServer(onRequest).listen(8888);
   console.log('Server has started...');
 }
 
